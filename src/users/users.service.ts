47288bdbdc838957b86836e097480ac1cfbb3b2f
tywhisky <fty221@gmail.com>
2023-07-06 21:37:16 +0800

    Add Swagger

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from '../../prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 import { User } from '@prisma/client';
 import { CreateUserDto } from './users.dto';
 
