282217deb459f7eb1f05e4663f3336b75651c956
tywhisky <fty221@gmail.com>
2023-08-15 11:48:04 +0800

    Add tasks table

---
+++ conf/evolutions/default/2.sql
@@ -0,0 +1,19 @@
+--- !Ups
+
+create table "task" (
+  "id" uuid not null primary key,
+  "title" varchar not null,
+  "description" text,
+  "prev_completed_at" timestamp,
+  "completed_at" timestamp,
+  "cycle_value" int,
+  "cycle_unit" cycle_unit_enum,
+  "userid" uuid not null,
+  foreign key ("userid") references "user"("id")
+);
+
+create type cycle_unit_enum as enum ('day', 'week', 'month', 'year');
+
+--- !Downs
+
+drop table "task" if exists;
