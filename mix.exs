f0f36999240c7478fa1dd01cb11940ddb9cf8cbd
tywhisky <fty221@gmail.com>
2024-06-09 15:47:44 +0800

    Install deps

--- mix.exs
+++ mix.exs
@@ -44,7 +44,9 @@ defmodule TodoService.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.2"}
+      {:bandit, "~> 1.2"},
+      {:absinthe, "~> 1.6"},
+      {:absinthe_plug, "~> 1.5"}
     ]
   end
 
